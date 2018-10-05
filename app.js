94722819a3af8c788bc5fdd63fdff686e59254d4
Raiyan Khan <raiyan.khan.106@gmail.com>
2018-10-05 20:25:48 +0600

    show view added, mongoose integrated

--- app.js
+++ app.js
@@ -2,10 +2,28 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const mongoose = require("mongoose");
 app.use(bodyParser.urlencoded({extended:true}));
 
+mongoose.connect("mongodb://localhost/winsome-canvas",{useNewUrlParser:true})
+        .then(()=>{console.log(`Connected to database`)})
+        .catch((err)=>{console.log(`Could not connect ${err}`)});
 
-//Places Array
+
+//Schema Set up
+
+const placeSchema = new mongoose.Schema({
+    name:String,
+    image:String,
+    description:String
+});
+
+const Places = mongoose.model("places",placeSchema);
+
+
+
+
+
 let places = [
     {
         name: 'Shangshad Bhaban',
@@ -18,32 +36,14 @@ let places = [
     {
         name: 'Bashundhara City',
         image:'http://amaryellowpages.com/wp-content/uploads/2013/12/AmarYellowPages-Bashundhara-City-1.jpg'
-    },
-    {
-        name: 'Shangshad Bhaban',
-        image:'https://media.dhakatribune.com/uploads/2013/06/JS-5.jpg'
-    },
-    {
-        name: 'LalBagh Kella',
-        image:'https://steemitimages.com/DQmZm9wc8BaawNs3SxiSNuVnqAepAo7mTVPo3taQoQ9f2LX/fort-lalbagh_raul-vibal-1000.jpg'
-    },
-    {
-        name: 'Bashundhara City',
-        image:'http://amaryellowpages.com/wp-content/uploads/2013/12/AmarYellowPages-Bashundhara-City-1.jpg'
-    },
-    {
-        name: 'Shangshad Bhaban',
-        image:'https://media.dhakatribune.com/uploads/2013/06/JS-5.jpg'
-    },
-    {
-        name: 'LalBagh Kella',
-        image:'https://steemitimages.com/DQmZm9wc8BaawNs3SxiSNuVnqAepAo7mTVPo3taQoQ9f2LX/fort-lalbagh_raul-vibal-1000.jpg'
-    },
-    {
-        name: 'Bashundhara City',
-        image:'http://amaryellowpages.com/wp-content/uploads/2013/12/AmarYellowPages-Bashundhara-City-1.jpg'
     }
-]
+];
+
+// Places.create({
+//     name: 'Shangshad Bhaban',
+//     image:'https://media.dhakatribune.com/uploads/2013/06/JS-5.jpg',
+//     description:"Parliament house of bangladesh, everyone is immune here :D"
+// });
 
 
 app.set('view engine', 'ejs');
@@ -52,22 +52,36 @@ app.get('/',(req,res)=>{
     res.render("landing");
 });
 
-app.get("/places", (req,res)=>{
-    res.render("places",{places:places});
+app.get("/places", async (req,res)=>{
+
+    const places = await Places.find();
+
+    res.render("index",{places:places});
 });
 
 app.get("/places/new",(req,res)=>{
     res.render("new.ejs");
 });
 
-app.post("/places",(req,res)=>{
-     let newPlace = {
+app.post("/places",async (req,res)=>{
+     const newPlace = new Places({
          name:req.body.name,
-         image:req.body.image
-     };
+         image:req.body.image,
+         description:req.body.description
+     });
 
-     places.push(newPlace);
+     await newPlace.save();
+     
      res.redirect("/places");
+});
+
+
+
+//SHOW route
+
+app.get("/places/:id", async (req,res)=>{
+    const place = await Places.findById(req.params.id);
+    res.render("show",{place:place});
 });
 
 
